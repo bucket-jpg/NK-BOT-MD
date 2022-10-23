485b7ac8c8ba699a03d155dc410ab867434b16b1
Matt_Melendez <76775569+NeKosmic@users.noreply.github.com>
2022-10-23 16:09:58 -0500

    ♻️🌌
    E

--- plugins/anime-info.js
+++ plugins/anime-info.js
@@ -4,24 +4,24 @@
 let handler=async(a,{conn:e,text:i})=>{if(!i)return reply(`Que anime desea buscar?, ejemplo de uso:
 
 ${Prefijo+command} nichijou
-`);let r=encodeURIComponent(i);try{let t=(await fetchJson(`https://api.jikan.moe/v4/anime?q=${r}`)).data[0],n=t.images.jpg.image_url?t.images.webp.image_url:t.images.jpg.large_image_url?t.images.webp.large_image_url:"https://github.com/NeKosmic/NK-BOT/raw/main/multimedia/imagenes/anim_vers.jpg",o=`[ ${t.title} - Comun ], [ ${t.title_english} - Ingles ], [ ${t.title_japanese} - Japon\xe9s ]`;try{var s=`*🔥 Productora:* ${t.producers[0].name||"-"}
-*🪀 Licenciado por:* ${t.licensors[0].name||"-"}
-*🌟 Estudio:* ${t.studios[0].name||"-"}`}catch{var s=""}try{var l=`*📺 Trailer:* ${t.trailer.url||"Url no encontrado!"}`}catch{var l=""}await e.sendMessage(a.chat,{image:{url:n},caption:`
-${"*\uD83E\uDDEC ID:* "+t.mal_id}
+`);let r;await a.reply(MultiNK.Bsqd(await e.getName(a.sender)));let t=encodeURIComponent(i);try{let n=(await fetchJson(`https://api.jikan.moe/v4/anime?q=${t}`)).data[0],s=n.images.jpg.image_url?n.images.webp.image_url:n.images.jpg.large_image_url?n.images.webp.large_image_url:"https://github.com/NeKosmic/NK-BOT/raw/main/multimedia/imagenes/anim_vers.jpg",o=`[ ${n.title} - Comun ], [ ${n.title_english} - Ingles ], [ ${n.title_japanese} - Japon\xe9s ]`;try{var l=`*🔥 Productora:* ${n.producers[0].name||"-"}
+*🪀 Licenciado por:* ${n.licensors[0].name||"-"}
+*🌟 Estudio:* ${n.studios[0].name||"-"}`}catch{var l=""}try{var m=`*📺 Trailer:* ${n.trailer.url||"Url no encontrado!"}`}catch{var m=""}await e.sendMessage(a.chat,{image:{url:s},caption:`
+${"*\uD83E\uDDEC ID:* "+n.mal_id}
 ${"*✍️ T\xedtulos:* "+o}
-${"*\uD83E\uDE84 Tipo:* "+t.type}
-${"*\uD83E\uDDE9 Genero:* "+t.source}
-${"*\uD83D\uDDC3️ Episodio:* "+t.episodes}
-${"*\uD83C\uDFAD Estado:* "+await traducIr(encodeURI(t.status))}
-${"*⌚ Duraci\xf3n:* "+await traducIr(encodeURI(t.duration))}
-${"*♻️ Clasificaci\xf3n:* "+await traducIr(encodeURI(t.rating))}
-${"*\uD83D\uDCC8 Puntaje:* "+t.score}
-${"*\uD83D\uDC4D Calificado por:* "+t.scored_by}
-${"*\uD83D\uDD16 Rango:* "+t.rank}
-${"*⚡ Popularidad:* "+t.popularity}
-${"*\uD83D\uDC65 Miembros:* "+t.members}
-${"*❤️ Favoritos:* "+t.favorites}
-${"*\uD83D\uDCDC Sinopsis:* "+await traducIr(encodeURI(t.synopsis))}
-${s}
+${"*\uD83E\uDE84 Tipo:* "+n.type}
+${"*\uD83E\uDDE9 Genero:* "+n.source}
+${"*\uD83D\uDDC3️ Episodio:* "+n.episodes}
+${"*\uD83C\uDFAD Estado:* "+await traducIr(encodeURI(n.status))}
+${"*⌚ Duraci\xf3n:* "+await traducIr(encodeURI(n.duration))}
+${"*♻️ Clasificaci\xf3n:* "+await traducIr(encodeURI(n.rating))}
+${"*\uD83D\uDCC8 Puntaje:* "+n.score}
+${"*\uD83D\uDC4D Calificado por:* "+n.scored_by}
+${"*\uD83D\uDD16 Rango:* "+n.rank}
+${"*⚡ Popularidad:* "+n.popularity}
+${"*\uD83D\uDC65 Miembros:* "+n.members}
+${"*❤️ Favoritos:* "+n.favorites}
+${"*\uD83D\uDCDC Sinopsis:* "+await traducIr(encodeURI(n.synopsis))}
 ${l}
-`.trim()},{quoted:a})}catch(m){a.reply(MultiNK.Error0())}};handler.help=["infoanime"],handler.tags=["animeuwu"],handler.command=/^(infoanime)$/i,handler.limit=!0;export default handler;
+${m}
+`.trim()},{quoted:a})}catch(d){a.reply(MultiNK.Error0())}};handler.help=["infoanime"],handler.tags=["animeuwu"],handler.command=/^(infoanime)$/i;export default handler;
