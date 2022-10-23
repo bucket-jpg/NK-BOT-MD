43b62a85f57df4ff47485b7132730bc1180a3879
Matt_Melendez <76775569+NeKosmic@users.noreply.github.com>
2022-10-22 19:48:44 -0500

    🌌♻️
    E

--- plugins/dl-mediafire.js
+++ plugins/dl-mediafire.js
@@ -1,6 +1,8 @@
-import{mediafiredl as e}from"@bochilteam/scraper";let handler=async(r,{conn:i,args:a,command:l})=>{if(!a[0])return r.reply("*[ ! ] Y el Link?*");if(!isUrl(a[0])&&!a[0].includes("https://www.mediafire.com/"))return r.reply("*[ ! ] Link invalido*\n_Por favor, use un link de MediaFire_");try{let{url:n,url2:t,filename:d,ext:m,aploud:o,filesize:c,filesizeH:h}=await e(a[0]),p=`
-*Nombre:* ${d}
+import{mediafiredl as e}from"@bochilteam/scraper";let handler=async(r,{conn:i,args:a,command:l})=>{if(!a[0])return r.reply("*[ ! ] Y el Link?*");if(!isUrl(a[0])&&!a[0].includes("https://www.mediafire.com/"))return r.reply("*[ ! ] Link invalido*\n_Por favor, use un link de MediaFire_");try{let n=await e(a[0]),{url:t,url2:d,filename:m,ext:o,aploud:p,filesize:c,filesizeH:h}=n,s=`
+*Nombre:* ${m}
 *Tama\xf1o:* ${h}
-*Extensi\xf3n:* ${m}
-*Fecha de subida:* ${o}
-`.trim();r.reply(p),await i.sendFile(r.chat,n,d,"",r,null,{mimetype:m,asDocument:!0})}catch(s){r.reply(MultiNK.Error1())}};handler.help=["mediafire"].map(e=>e+" <link>"),handler.tags=["servicio"],handler.command=/^(mediafire)$/i,handler.limit=!0;export default handler;
+*Extensi\xf3n:* ${o}
+*Fecha de subida:* ${p}
+${"*Tipo de archivo :* "+n.filetype}
+${"*Url :* "+d}
+`.trim();if(r.reply(s),c>5e4)return r.reply(MultiNK.Error1());await i.sendFile(r.chat,t,m,"",r,null,{mimetype:o,asDocument:!0})}catch($){r.reply(MultiNK.Error0())}};handler.help=["mediafire"].map(e=>e+" <link>"),handler.tags=["servicio"],handler.command=/^(mediafire)$/i,handler.limit=!0;export default handler;
