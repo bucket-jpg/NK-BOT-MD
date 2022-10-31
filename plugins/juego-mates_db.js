ccabe2f2f243059530909c69cbd196aae21572fd
Matt_Melendez <76775569+NeKosmic@users.noreply.github.com>
2022-10-30 19:41:14 -0500

    ♻️🌌
    E

--- plugins/juego-mates_db.js
+++ plugins/juego-mates_db.js
@@ -1,28 +1,6 @@
-import db from '../lib/database.js'
-
-let handler = m => m
-handler.before = async function (m) {
-    if (!/^-?[0-9]+(\.[0-9]+)?$/.test(m.text)) return !0
-    let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.text || !/^¿Cuál es el resultado de/i.test(m.quoted.text)) return !0
-    this.math = this.math ? this.math : {}
-    if (!(id in this.math)) return this.sendMessage(m.chat, {text: 'La pregunta ya fue resuelta...'}, {quoted: m})
-    if (m.quoted.id == this.math[id][0].id) {
-        let math = JSON.parse(JSON.stringify(this.math[id][1]))
-        if (m.text == math.result) {
-            db.data.users[m.sender].exp += math.bonus
-            clearTimeout(this.math[id][3])
-            delete this.math[id]
-            this.sendButton(m.chat, `*Respuesta correcta!*\nAqui tienes tu recompensa:\n+${math.bonus} XP\n`, NombreDelBot, null, [['SIGUIENTE', Prefijo+`mates ${math.mode}`]], m)
-        } else {
-            if (--this.math[id][2] == 0) {
-                clearTimeout(this.math[id][3])
-                delete this.math[id]
-                m.reply(`*[ ! ] Se acabo el tiempo*\nRespuesta: *${math.result}*`)
-            } else m.reply(`*Respuesta incorrecta!*\nTe quedan ${this.math[id][2]} oportunidades`)
-        }
-    }
-    return !0
-}
-
-export default handler
+import t from"../lib/database.js";let handler=t=>t;handler.before=async function(e){if(!/^-?[0-9]+(\.[0-9]+)?$/.test(e.text))return!0;let a=e.chat;if(!e.quoted||!e.quoted.fromMe||!e.text||!/^¿Cuál es el resultado de/i.test(e.quoted.text))return!0;if(this.math=this.math?this.math:{},!(a in this.math))return this.sendMessage(e.chat,{text:"La pregunta ya fue resuelta..."},{quoted:e});if(e.quoted.id==this.math[a][0].id){let s=JSON.parse(JSON.stringify(this.math[a][1]));e.text==s.result?(t.data.users[e.sender].exp+=s.bonus,clearTimeout(this.math[a][3]),delete this.math[a],this.sendButton(e.chat,`*Respuesta correcta!*
+Aqui tienes tu recompensa:
++${s.bonus} XP
+`,NombreDelBot,null,[["SIGUIENTE",Prefijo+`mates ${s.mode}`]],e)):0==--this.math[a][2]?(clearTimeout(this.math[a][3]),delete this.math[a],e.reply(`*[ ! ] Se acabo el tiempo*
+Respuesta: *${s.result}*`)):await e.reply(`*Respuesta incorrecta!*
+Te quedan ${this.math[a][2]} oportunidades`)}return!0};export default handler;
